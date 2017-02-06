75a125fbef11087af574c9987b0ba9e2f8fe5811
Rama Milaneh <ramamilaneh@gmail.com>
2017-02-06 13:43:17 -0500

    First commit

---
+++ Realm-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm-example
+//
+//  Created by Rama Milaneh on 2/6/17.
+//  Copyright © 2017 Rama Milaneh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
