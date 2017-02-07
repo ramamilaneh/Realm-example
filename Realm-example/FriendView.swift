5bd4bd006ac6a4e399af3740149c0c2c2bf00aa0
Rama Milaneh <ramamilaneh@gmail.com>
2017-02-07 08:21:33 -0500

    Second Commit

---
+++ Realm-example/FriendView.swift
@@ -0,0 +1,16 @@
+//
+//  FriendView.swift
+//  Realm-example
+//
+//  Created by Rama Milaneh on 2/6/17.
+//  Copyright © 2017 Rama Milaneh. All rights reserved.
+//
+
+import UIKit
+
+class FriendView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    
+
+}
